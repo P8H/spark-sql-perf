ca25b18fc02a2874ddd109eb2bc1e210f0e2ff4a
Kevin Rojczyk <kevin.roj@p8h.de>
2017-05-25 01:33:58 +0200

    improve autotune tpcds

--- src/main/scala/AutoTuneTPCDS.scala
+++ src/main/scala/AutoTuneTPCDS.scala
@@ -3,39 +3,90 @@ import org.autotune.exampleConfigs.SparkTuneableConf
 import org.autotune._
 import org.scalatest.FunSuite
 import com.databricks.spark.sql.perf.tpcds.TPCDS
+import com.databricks.spark.sql.perf.tpcds.Tables
+import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.functions._
 
 class AutoTuneTPCDS extends FunSuite {
 
-  test("pop is invoked on an empty stack") {
-    val tuner = new AutoTuneDefault[SparkTuneableConf](new SparkTuneableConf)
+  private def getSparkBuilder(): SparkSession.Builder ={
+    return SparkSession.builder.
+      appName("TPCDS AutoTune Benchmark")
+      .master("local[*]")
+      .config("spark.sql.perf.results", "/Users/KevinRoj/Desktop/testdata"); //directory for results
+  }
 
-    val cfg = tuner.start.getConfig
-    val spark = cfg
-      .setConfig(SparkSession.builder.appName("Java Spark SQL data sources example").master("local[*]"))
-      .config("spark.sql.perf.results", "/Users/KevinRoj/Desktop/testdata")
-      .getOrCreate
+  private def reduceLogLevel(spark: SparkSession) = {
+    val sparkContextD = JavaSparkContext.fromSparkContext(spark.sparkContext)
+    sparkContextD.setLogLevel("ERROR")
+  }
 
+  private def runBenchmark(spark: SparkSession) : Double = {
     val sqlContext = spark.sqlContext
 
-    import com.databricks.spark.sql.perf.tpcds.Tables
     // Tables in TPC-DS benchmark used by experiments.
     // dsdgenDir is the location of dsdgen tool installed in your machines.
     // scaleFactor - Volume of data to generate in GB
     val tables = new Tables(sqlContext, "/Users/KevinRoj/Desktop/tpcds-kit-master/tools", 1)
     // Generate data.
     //tables.genData("/Volumes/Externe SSD/BigData", "parquet", true, true, true, true, false)
-    // Create metastore tables in a specified database for your data.
-    // Once tables are created, the current database will be switched to the specified database.
-    //tables.createExternalTables("/Volumes/Externe SSD/BigData", format, databaseName, overwrite)
-    // Or, if you want to create temporary tables
+
     tables.createTemporaryTables("/Volumes/Externe SSD/BigData", "parquet")
-    // Setup TPC-DS experiment
-
     val tpcds = new TPCDS(sqlContext = sqlContext)
-
     val experiment = tpcds.runExperiment(tpcds.interactiveQueries)
     experiment.waitForFinish(60*60*10)
 
-    val resultTable = spark.read.json(experiment.resultPath).select("iteration", "results").show()
+    //get result
+    val runtime = spark.read.json(experiment.resultPath)
+      .select(col("iteration"), org.apache.spark.sql.functions.explode(col("results")))
+      .withColumn("TOTAL", expr("col.analysisTime + col.executionTime + col.optimizationTime + col.parsingTime + col.planningTime"))
+      .select(sum("TOTAL")).first().asInstanceOf[GenericRowWithSchema].getDouble(0)
+
+    return runtime
+  }
+
+  test("autotune benchmark") {
+    val tuner = new AutoTuneDefault[SparkTuneableConf](new SparkTuneableConf)
+
+    {
+      val cfg = new SparkTuneableConf //warm up with default configuration //tuner.start.getConfig
+      val spark = cfg.setConfig(getSparkBuilder()).getOrCreate
+      val sqlContext = spark.sqlContext
+
+      val tables = new Tables(sqlContext, "/Users/KevinRoj/Desktop/tpcds-kit-master/tools", 1)
+      // Generate data.
+      //tables.genData("/Volumes/Externe SSD/BigData", "parquet", true, true, true, true, false)
+
+      tables.createTemporaryTables("/Volumes/Externe SSD/BigData", "parquet")
+      val tpcds = new TPCDS(sqlContext = sqlContext)
+      val experiment = tpcds.runExperiment(tpcds.interactiveQueries)
+      experiment.waitForFinish(60*60*10)
+    }
+
+
+
+    /** tune section - search for the best configuration **/
+
+    var t: Int = 0
+    while ( {
+      t < 40
+    }) { //40 benchmark tests
+      val cfg: SparkTuneableConf = tuner.start.getConfig
+      val spark: SparkSession = cfg.setConfig(getSparkBuilder).getOrCreate
+      reduceLogLevel(spark)
+
+      val cost = runBenchmark(spark)
+      spark.stop()
+
+      tuner.addCost(cost)
+      tuner.end()
+
+      {
+        t += 1; t - 1
+      }
+    }
+
+
   }
 }
